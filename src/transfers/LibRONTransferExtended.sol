7e3786b4d4ff625c64cf13b1a8067fa3efe99fb3
huyhuynh3103 <huy.huynh@skymavis.com>
2023-12-30 15:17:53 +0700

    feat: add ron transfer libs

---
+++ src/transfers/LibRONTransferExtended.sol
@@ -0,0 +1,122 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../interfaces/IWRON.sol";
+import "../interfaces/IWRONHelper.sol";
+import "./LibRONTransfer.sol";
+import "./LibTokenTransferFrom.sol";
+import "./LibTokenTransfer.sol";
+
+library LibRONTransferExtended {
+  /**
+   * @dev Safely unwraps tokens if it is WRON token and transfers them to a specified recipient.
+   * @param _wron Address of the WRON contract.
+   * @param _token Address of the ERC20 token to unwrap and transfer.
+   * @param _to Address of the recipient to transfer the tokens to.
+   * @param _amount Amount of tokens to transfer.
+   *
+   * Note: This function may cause a revert if the consumer contract is a proxy, consider using the method
+   * `safeUnwrapTokenAndTransferWithHelper` instead.
+   */
+  function safeUnwrapTokenAndTransfer(IWRON _wron, address _token, address payable _to, uint256 _amount) internal {
+    if (_token == address(_wron)) {
+      // Check whether the recipient receives RON
+      if (LibRONTransfer.send(_to, 0)) {
+        _wron.withdraw(_amount);
+        LibRONTransfer.safeTransfer(_to, _amount);
+        return;
+      }
+    }
+
+    LibTokenTransfer.safeTransfer(_token, _to, _amount);
+  }
+
+  /**
+   * @dev See `safeUnwrapTokenAndTransfer`.
+   *
+   * Requirements:
+   * - The consumer contract must approve the contract `_wronHelper`.
+   *
+   * Note: This function supports the use of a proxy contract by using the WRONHelper contract to unwrap WRON and
+   * transfer RON.
+   */
+  function safeUnwrapTokenAndTransferWithHelper(
+    IWRON _wron,
+    IWRONHelper _wronHelper,
+    address _token,
+    address payable _to,
+    uint256 _amount
+  ) internal {
+    if (_token == address(_wron)) {
+      // Check whether the recipient receives RON
+      if (LibRONTransfer.send(_to, 0)) {
+        _wron.approve(address(_wronHelper), _amount);
+        _wronHelper.withdrawTo(_to, _amount);
+        return;
+      }
+    }
+
+    LibTokenTransfer.safeTransfer(_token, _to, _amount);
+  }
+
+  /**
+   * @dev Safely unwraps tokens if it is WRON token from specified sender, and transfers them to a specified recipient.
+   * @param _wron Address of the WRON contract.
+   * @param _token Address of the ERC20 token to unwrap and transfer.
+   * @param _from Address of the sender on whose behalf the tokens will be unwrapped and transferred.
+   * @param _to Address of the recipient to transfer the tokens to.
+   * @param _amount Amount of tokens to transfer.
+   *
+   * Note: This function may cause a revert if the consumer contract is a proxy, consider using the method
+   * `safeUnwrapTokenAndTransferFromWithHelper` instead.
+   */
+  function safeUnwrapTokenAndTransferFrom(
+    IWRON _wron,
+    address _token,
+    address _from,
+    address payable _to,
+    uint256 _amount
+  ) internal {
+    if (_token == address(_wron)) {
+      // Check whether the recipient receives RON
+      if (LibRONTransfer.send(_to, 0)) {
+        LibTokenTransferFrom.safeTransferFrom(_token, _from, address(this), _amount);
+        IWRON(_wron).withdraw(_amount);
+        LibRONTransfer.safeTransfer(_to, _amount);
+        return;
+      }
+    }
+
+    LibTokenTransferFrom.safeTransferFrom(_token, _from, _to, _amount);
+  }
+
+  /**
+   * @dev See `safeUnwrapTokenAndTransfer`.
+   *
+   * Requirements:
+   * - The consumer contract must approve the contract `_wronHelper`.
+   *
+   * Note: This function supports the use of a proxy contract by using the WRONHelper contract to unwrap WRON and
+   * transfer RON.
+   */
+  function safeUnwrapTokenAndTransferFromWithHelper(
+    IWRON _wron,
+    IWRONHelper _wronHelper,
+    address _token,
+    address _from,
+    address payable _to,
+    uint256 _amount
+  ) internal {
+    if (_token == address(_wron)) {
+      // Check whether the recipient receives RON
+      if (LibRONTransfer.send(_to, 0)) {
+        LibTokenTransferFrom.safeTransferFrom(_token, _from, address(this), _amount);
+        _wron.approve(address(_wronHelper), _amount);
+        _wronHelper.withdrawTo(_to, _amount);
+        return;
+      }
+    }
+
+    LibTokenTransferFrom.safeTransferFrom(_token, _from, _to, _amount);
+  }
+}
