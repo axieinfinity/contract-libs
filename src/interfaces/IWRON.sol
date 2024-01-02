adb78b043ae8c1a437dfd8ade94151ddbed5cfc6
huyhuynh3103 <huy.huynh@skymavis.com>
2024-01-02 13:29:13 +0700

    chore: use relative import and add docs

--- src/interfaces/IWRON.sol
+++ src/interfaces/IWRON.sol
@@ -2,15 +2,25 @@
 pragma solidity ^0.8.0;
 
 interface IWRON {
+  /**
+   * @dev Deposit RON and increase balance WRON tokens of sender.
+   */
   function deposit() external payable;
 
+  /// @dev See {IERC20-transfer}.
   function transfer(address to, uint256 value) external returns (bool);
 
+  /// @dev See {IERC20-transferFrom}.
   function transferFrom(address src, address dst, uint256 wad) external returns (bool);
 
+  /**
+   * @dev Withdraw RON and decrease balance WRON tokens of sender.
+   */
   function withdraw(uint256) external;
 
+  /// @dev See {IERC20-balanceOf}.
   function balanceOf(address) external view returns (uint256);
 
+  /// @dev See {IERC20-approve}.
   function approve(address guy, uint256 wad) external returns (bool);
 }
--- src/interfaces/IWRONHelper.sol
+++ src/interfaces/IWRONHelper.sol
@@ -2,9 +2,24 @@
 pragma solidity ^0.8.0;
 
 interface IWRONHelper {
+  /**
+   * @dev Returns WRON address.
+   */
   function WRON() external view returns (address);
 
+  /**
+   * @dev Deposit WRON and withdraw RON token.
+   *
+   * Requirements:
+   * - This contract must be approved `amount` WRON tokens by sender.
+   */
   function withdraw(uint256 amount) external;
 
+  /**
+   * @dev Deposit WRON and withdraw RON token to address `to`.
+   *
+   * Requirements:
+   * - This contract must be approved `amount` WRON tokens by sender.
+   */
   function withdrawTo(address payable to, uint256 amount) external;
 }
