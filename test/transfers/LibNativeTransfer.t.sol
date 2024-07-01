d0019c04f65be24d884294b9bd440a63354c77d3
huyhuynh3103 <huy.huynh@skymavis.com>
2024-07-01 13:56:30 +0700

    chore: using relative path instead of remapping

--- test/transfers/LibNativeTransfer.t.sol
+++ test/transfers/LibNativeTransfer.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import { Test } from "@forge-std-1.8.2/Test.sol";
+import { Test } from "../../dependencies/forge-std-1.8.2/src/Test.sol";
 import { LibNativeTransfer } from "src/transfers/LibNativeTransfer.sol";
 
 contract LibNativeTransferTest is Test {
