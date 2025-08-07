bd8279701baa42a8b118e6097d79592449473e0f
huyhuynh3103 <huy.huynh@skymavis.com>
2025-08-07 15:18:10 +0700

    chore: add comments and minor improvements

--- src/nonce/UnorderedNonceBitmapUpgradeable.sol
+++ src/nonce/UnorderedNonceBitmapUpgradeable.sol
@@ -26,8 +26,8 @@ abstract contract UnorderedNonceBitMapUpgradeable {
 
   /// @dev Check if the nonce has been used before.
   function isUsedNonce(address from, uint256 nonce) public view returns (bool) {
-    (uint256 wordPos, uint256 bitPos) = _bitmapPositions(nonce);
-    uint256 bitMask = 1 << bitPos;
+    (uint248 wordPos, uint8 bitPos) = _bitmapPositions(nonce);
+    uint256 bitMask = _toBitMask(bitPos);
     uint256 flipped = _getUnorderedNonceBitmapStorage()._nonceBitmap[from][wordPos] ^ bitMask;
     return flipped & bitMask == 0;
   }
@@ -40,8 +40,8 @@ abstract contract UnorderedNonceBitMapUpgradeable {
   /// @dev Try to mark the nonce as used, return false if it has been used before.
   function _tryUseUnorderedNonce(address from, uint256 nonce) internal returns (bool) {
     UnorderedNonceBitmapStorage storage $ = _getUnorderedNonceBitmapStorage();
-    (uint256 wordPos, uint256 bitPos) = _bitmapPositions(nonce);
-    uint256 bitMask = 1 << bitPos;
+    (uint248 wordPos, uint8 bitPos) = _bitmapPositions(nonce);
+    uint256 bitMask = _toBitMask(bitPos);
     uint256 flipped = $._nonceBitmap[from][wordPos] ^ bitMask;
 
     if (flipped & bitMask != 0) {
@@ -53,9 +53,20 @@ abstract contract UnorderedNonceBitMapUpgradeable {
     return false;
   }
 
+  /// @notice Returns the index of the bitmap and the bit position within the bitmap. Used for unordered nonces
+  /// @param nonce The nonce to get the associated word and bit positions
+  /// @return wordPos The word position or index into the nonceBitmap
+  /// @return bitPos The bit position
+  /// @dev The first 248 bits of the nonce value is the index of the desired bitmap
+  /// @dev The last 8 bits of the nonce value is the position of the bit in the bitmap
   function _bitmapPositions(uint256 nonce) private pure returns (uint248 wordPos, uint8 bitPos) {
     wordPos = uint248(nonce >> 8);
     bitPos = uint8(nonce);
+  }
+
+  /// @dev Turn on the bit at the given position.
+  function _toBitMask(uint8 bitPos) internal pure returns (uint256 mask) {
+    mask = 1 << bitPos;
   }
 
   function _getUnorderedNonceBitmapStorage() private pure returns (UnorderedNonceBitmapStorage storage $) {
